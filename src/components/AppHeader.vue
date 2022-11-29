e9430d15dd78db74559ca157cd898503827050d3
heoisme <105918985+heoismeee@users.noreply.github.com>
2022-11-29 14:17:18 +0700

    Add files via upload

---
+++ src/components/AppHeader.vue
@@ -0,0 +1,11 @@
+<template>
+    <nav class="navbar navbar-expand-lg navbar-dark ftco_navbar bg-dark ftco-navbar-light" id="ftco-navbar">
+	    <div class="container">
+	      <a class="navbar-brand" href="index.html"><span class="flaticon-pizza-1 mr-1"></span>Pizza<br><small>Delicous</small></a>
+		</div>
+	  </nav>
+</template>
+
+<style>
+
+</style>
